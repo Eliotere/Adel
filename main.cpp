b01766e8820bdbd129d59060fc50825adb867b58
Eliot <eliot@lopezgallou.com>
2024-09-28 16:36:19 +0200

    SDL test works, careful with the Cmake which can create problems

---
+++ main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+#include <SDL2/SDL.h>
+
+int main()
+{
+
+    std::cout << "Hello Wrold!" << std::endl;
+
+    if(SDL_Init(SDL_INIT_VIDEO) < 0)
+    {
+        std::cout << "Failed to initialize the SDL2 library\n";
+        return -1;
+    }
+
+    SDL_Window *window = SDL_CreateWindow("SDL2 Window",
+                                          SDL_WINDOWPOS_CENTERED,
+                                          SDL_WINDOWPOS_CENTERED,
+                                          680, 480,
+                                          0);
+
+    if(!window)
+    {
+        std::cout << "Failed to create window\n";
+        return -1;
+    }
+
+    SDL_Surface *window_surface = SDL_GetWindowSurface(window);
+
+    if(!window_surface)
+    {
+        std::cout << "Failed to get the surface from the window\n";
+        return -1;
+    }
+
+    SDL_UpdateWindowSurface(window);
+
+    SDL_Delay(5000);
+    
+}
